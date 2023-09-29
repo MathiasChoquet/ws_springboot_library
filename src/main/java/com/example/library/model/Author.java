14430f0a94cf93c0dc40752aa59fa7a2691c7f2c
Mathias CHOQUET <mathias.choquet@gmail.com>
2023-09-29 15:00:27 +0200

    add sequence

--- src/main/java/com/example/library/model/Author.java
+++ src/main/java/com/example/library/model/Author.java
@@ -2,6 +2,8 @@ package com.example.library.model;
 
 import java.util.List;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -16,7 +18,8 @@ import jakarta.persistence.Table;
 public class Author {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seqAuthor")
+    @GenericGenerator(name = "seqAuthor", strategy = "increment")
     private Long id;
 
     @Column(name = "name")
